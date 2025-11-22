b554d78b23e545638a04b634b3c36ad518d77d1f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-11-21 22:09:10 -0300

    bot telegram

---
+++ 20251121-aula08/bot_telegram.py
@@ -0,0 +1,41 @@
+import datetime
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import Bot, Update
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+class AutomacaoProwayBot(Bot):
+    def __init__(self):
+        return super().__init__(os.getenv("TELEGRAM_TOKEN"))
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+    user_name = update.message.from_user.first_name
+
+    timestamp = datetime.datetime.now(datetime.UTC).strftime(
+        "%d/%m/%Y %H:%M:%S"
+    )
+
+    await update.message.reply_text(f"Olá {user_name}. Você digitou '{user_text}'. Agora são {timestamp}.")
+
+bot_app = ApplicationBuilder().bot(AutomacaoProwayBot()).build()
+bot_app.add_handler(MessageHandler(filters.TEXT, echo))
+
+if __name__ == "__main__":
+    logger.info("Iniciando bot em modo POLLING...")
+    bot_app.run_polling()
